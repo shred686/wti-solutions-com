8b8aaf65fad69334afe5decda487d2a9ce2d2625
Michael Kerner <michael.kerner@wti-solutions.com>
2025-08-27 16:51:06 -0400

    configure for GitHub pages

---
+++ astro.config.dev.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+// Development config - no base path
+export default defineConfig({
+  integrations: [tailwind()]
+}); 
