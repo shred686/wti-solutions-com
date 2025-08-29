5fd2d748b707226872057fac7c24371b9b42a99d
Michael Kerner <michael.kerner@wti-solutions.com>
2025-08-28 22:53:52 -0400

    home page work

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://shred686.github.io',
-  base: process.env.GITHUB_PAGES === 'true' ? '/wti-solutions-com' : undefined
+  base: process.env.GITHUB_PAGES === 'true' ? '/wti-solutions-com/' : undefined
 });
